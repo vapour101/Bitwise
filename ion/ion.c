11c4b2f6001f03e78a284366061294c68edd702b
Vapour101 <vapour101@gmail.com>
2018-03-17 19:15:14 +0200

    Stretchy Buffers Implemented

--- ion/ion.c
+++ ion/ion.c
@@ -1,7 +1,67 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(x, y) ((x) >= (y) ? (x) : (y))
+
+// stretchy buffers
+
+typedef struct BufHdr {
+    size_t len;
+    size_t cap;
+    char buf[0];
+} BufHdr;
+
+#define buf__hdr(b) ((BufHdr*)((char*)b - offsetof(BufHdr, buf)))
+#define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
+#define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow((b), buf_len(b) + (n), sizeof(*(b)))))
+
+#define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
+#define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
+#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
+#define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
+
+void* buf__grow(const void* buf, size_t new_len, size_t elem_size)
+{
+    size_t new_cap = MAX(1 + 2 * buf_cap(buf), new_len);
+    assert(new_len <= new_cap);
+    size_t new_size = offsetof(BufHdr, buf) + new_cap * elem_size;
+
+    BufHdr* new_hdr;
+    if (buf) {
+        new_hdr = realloc(buf__hdr(buf), new_size);
+    } else {
+        new_hdr = malloc(new_size);
+        new_hdr->len = 0;
+    }
+
+    new_hdr->cap = new_cap;
+    return new_hdr->buf;
+}
+
+void buf_test()
+{
+    int* test_buffer = NULL;
+
+    enum { N = 1024 };
+
+    for (int i = 0; i < N; i++) {
+        buf_push(test_buffer, i);
+    }
+
+    assert(buf_len(test_buffer) == N);
+
+    for (int i = 0; i < N; i++) {
+        assert(test_buffer[i] == i);
+    }
+
+    buf_free(test_buffer);
+}
 
 int main(int argc, char** argv)
 {
-  printf("Hello, world!\n");
-  return 0;
+    buf_test();
+    return 0;
 }
